21a3911e87cda004f0dae26b79e9c7a863a67b06
Troy Kelly <troy@troykelly.com>
2024-06-06 15:41:51 +1000

    Update entrypoint.sh

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -3,22 +3,21 @@
 # Exit immediately if any command exits with a non-zero status
 set -e
 
-# Check if PORT environment variable is set, if not, default to 3000
+# Check if the PORT environment variable is set, if not, default to 3000
 PORT=${PORT:-3000}
 
-# Log the start-up configuration
+# Log the startup configuration
 echo "Starting Gunicorn on port $PORT"
 
 # Check if the first argument is a flag or a command
-if [ "${1:0:1}" = '-' ]; then
+if [ "${1#-}" != "$1" ]; then
   # Assume user is passing Gunicorn flags, prepend Gunicorn command
   set -- gunicorn -b 0.0.0.0:${PORT} "$@"
 fi
 
 # If the user passes a command (not starting with `gunicorn`), run that instead.
-if [ "$1" != "gunicorn" ]; then
-  exec "$@"
-else
-  # Execute Gunicorn server with expanded environment variables
+if [ "$1" = "gunicorn" ]; then
   exec gunicorn -b 0.0.0.0:${PORT} "$@"
+else
+  exec "$@"
 fi
