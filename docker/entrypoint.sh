6bdf9f47c5b46495e36811621ac98a4a2bf0ba8b
Troy Kelly <troy@troykelly.com>
2024-06-07 15:50:10 +1000

    Updating to multithread I guess?

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -7,7 +7,7 @@ set -e
 PORT=${PORT:-3000}
 
 # Log the startup configuration
-echo "Starting Gunicorn on port $PORT"
+echo "Starting Gunicorn on port $PORT with a timeout of 120s with 4 workers"
 
 # Bind to both IPv4 and IPv6 addresses on the specified port
 BIND_ADDR="[::]:${PORT}"
@@ -15,12 +15,12 @@ BIND_ADDR="[::]:${PORT}"
 # Check if the first argument is a flag or a command
 if [ "${1#-}" != "$1" ]; then
     # Assume user is passing Gunicorn flags, prepend Gunicorn command
-    set -- gunicorn -b ${BIND_ADDR} src.server:app "$@"
+    set -- gunicorn -b ${BIND_ADDR} --timeout 120 --workers 4 --worker-class gevent src.server:app "$@"
 fi
 
 # If the user passes a command (not starting with `gunicorn`), run that instead.
 if [ "$1" = "gunicorn" ]; then
-    exec gunicorn -b ${BIND_ADDR} src.server:app "$@"
+    exec gunicorn -b ${BIND_ADDR} --timeout 120 --workers 4 --worker-class gevent src.server:app "$@"
 else
     exec "$@"
 fi
